7d3e80e243aa31cbf309f496353617d24fa1150d
Charles Butterworth <charlessbutterworth@gmail.com>
2020-12-08 15:27:10 -0600

    added in the state dropwdown within the plan trip

--- src/components/parks/ParkList.js
+++ src/components/parks/ParkList.js
@@ -13,7 +13,7 @@ export const ParkList = (props) => {
 
     return (
     <div className="parkList">
-        <h1>Parks</h1>
+        <h1>Select a Park</h1>
             <select className="dropdown" id="parkSelect">
                 <option value="0">Please select a park...</option>
                 {parks.map(
@@ -25,9 +25,23 @@ export const ParkList = (props) => {
             }
             </select>
         `
+        <div className="stateList">
+        <h1>Select a State</h1>
+            <select className="dropdown" id="stateSelect">
+                <option value="0">Please select a state</option>
+                {parks.map(
+                    parksObj => {
+                        return `<option value="${parksObj.fullName}">${parksObj.fullName}</option>`
+                        
+                    }
+                    ).join("")
+                }
+            </select>
+        `
 
     
         
+                </div>
         </div>
         )
 }
